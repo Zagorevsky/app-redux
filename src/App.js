f7bcb894902f1fa8259ecb41891e3f833e6204a8
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-06-14 15:18:58 +0300

    fix: gitignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function App() {
+
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD", payload: cash, })
+  }
+  const getCash = (cash) => {
+    dispatch({ type: "GET", payload: cash, })
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Счет:{ cash }</div>
+      <button onClick={ () => addCash(Number(prompt())) }>Внести на счет</button>
+      <button onClick={ () => getCash(Number(prompt())) }>Снять со счета</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+import {Provider} from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const defaultState = {
+  cash: 0,
+}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { ...state, cash: state.cash + action.payload }
+
+    case "GET":
+      return { ...state, cash: state.cash - action.payload }
+
+    default:
+      return state
+  }
+
+}
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+  document.getElementById('root')
+)
